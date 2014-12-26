a25ca024b2149560e44db51b30ff476a9be79ff2
bonzo <dani.speh@gmail.com>
2014-12-26 01:42:55 +0100

    Initial

---
+++ src/chapter02/liang_02_01.java
@@ -0,0 +1,13 @@
+package chapter02;
+
+/**
+ * Created by bonzo on 12/26/14.
+ */
+
+public class liang_02_01 {
+    public static void main(String[] args) {
+
+        System.out.println("asdasffasdf");
+    }
+
+}
