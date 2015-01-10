86430fd69015f2d205a94d8e32ff38fad430ce95
bonzo <dani.speh@gmail.com>
2015-01-10 05:35:33 +0100

    Initial

--- src/chapter02/liang_02_01.java
+++ src/chapter02/liang_02_01.java
@@ -1,5 +1,7 @@
 package chapter02;
 
+import java.util.Scanner;
+
 /**
  * Created by bonzo on 12/26/14.
  */
@@ -7,7 +9,11 @@ package chapter02;
 public class liang_02_01 {
     public static void main(String[] args) {
 
-        System.out.println("asdasffasdf");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a degree in Celsius: ");
+        double celsius = input.nextDouble();
+        double fahrenheit = (9.0 / 5.0) * celsius + 32;
+        System.out.println(celsius + " degree Celsius in Fahrenheit is: " + fahrenheit);
     }
 
 }
